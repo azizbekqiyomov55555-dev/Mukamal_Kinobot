77683fed865bded5a35039cceddb20e8b946bef7
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-03-20 00:46:35 +0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = "8655776547:AAEKHHQfCjvdwIgn_y4PH7de-b3g2Jd5iYs"
-ADMIN_IDS = [8537782289]  # Admin Telegram ID larini shu yerga qo'ying
-PAYMENT_CARD = "5614681872672690"  # To'lov karta raqami
-DB_PATH = "bot_database.db"
