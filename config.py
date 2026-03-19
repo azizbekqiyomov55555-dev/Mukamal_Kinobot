625f867c5ef42b29310680001ca162f662bad6c6
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-03-20 00:39:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-ADMIN_IDS = [123456789]  # Admin Telegram ID larini shu yerga qo'ying
-PAYMENT_CARD = "8600 0000 0000 0000"  # To'lov karta raqami
+BOT_TOKEN = "8655776547:AAEKHHQfCjvdwIgn_y4PH7de-b3g2Jd5iYs"
+ADMIN_IDS = [8537782289]  # Admin Telegram ID larini shu yerga qo'ying
+PAYMENT_CARD = "5614681872672690"  # To'lov karta raqami
 DB_PATH = "bot_database.db"
